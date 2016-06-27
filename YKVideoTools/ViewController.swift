a141408078b03d19545280cb79a79bdcada6a62f
youngkook <youngkook1992@163.com>
2016-06-27 14:14:48 +0800

    Initial Commit

---
+++ YKVideoTools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YKVideoTools
+//
+//  Created by Youngkook on 16/6/27.
+//  Copyright © 2016年 youngkook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YKVideoToolsTests/YKVideoToolsTests.swift
@@ -0,0 +1,36 @@
+//
+//  YKVideoToolsTests.swift
+//  YKVideoToolsTests
+//
+//  Created by Youngkook on 16/6/27.
+//  Copyright © 2016年 youngkook. All rights reserved.
+//
+
+import XCTest
+@testable import YKVideoTools
+
+class YKVideoToolsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
