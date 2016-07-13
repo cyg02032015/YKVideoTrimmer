96fd4f916ba53445281ceeaf9395abfffdcc03bb
youngkook <youngkook1992@163.com>
2016-07-13 18:32:15 +0800

    delete inject

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/song/Desktop/YKVideoTools/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/song/Desktop/YKVideoTools/iOSInjectionProject/BundleContents.h
